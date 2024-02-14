c9edadc37deb5118b73249dd31a48a13c5e0f491
Fred Teumer <fredteumer@gmail.com>
2024-02-14 13:35:03 -0500

    updating script to get node stakes, add verbosity and convert result to POKT from uPOKT

--- treasury-balance.sh
+++ treasury-balance.sh
@@ -2,7 +2,13 @@
 
 # Initialize the running sum
 total_sum=0
-POKT=https://mainnet.rpc.grove.city/v1/43397ae8
+total_sum_POKT=0
+
+# conversion factor for uPOKT -> POKT
+conversion=1000000
+
+# modify to a valid pocket RPC url. get one FREE: https://portal.grove.city
+POKT="Get an endpoint: https://portal.grove.city"
 
 # Loop through each line in the text file
 while IFS= read -r addr; do
@@ -20,19 +26,33 @@ while IFS= read -r addr; do
     # Convert balance to numeric value
     balance=$((balance))
     echo "Found $balance uPOKT in balance"
+    echo ""
    
-    # get staked token amounts...
+    # get app staked token amounts...
     output2=$(pocket query app "$addr" --remoteCLIURL "$POKT" | sed 1d)
-    staked=$(echo "$output2" | jq -r '.staked_tokens')
+    appstaked=$(echo "$output2" | jq -r '.staked_tokens')
 
     # convert staked to numeric
-    staked=$((staked))
-    echo "Found $staked staked uPOKT"
+    appstaked=$((appstaked))
+    echo "Found $appstaked staked uPOKT (AppStake)"
+    echo ""
+
+    # get node staked token amounts...
+    output2=$(pocket query node "$addr" --remoteCLIURL "$POKT" | sed 1d)
+    nodestaked=$(echo "$output2" | jq -r '.tokens')
+
+    # convert staked to numeric
+    nodestaked=$((nodestaked))
+    echo "Found $nodestaked staked uPOKT (NodeStake)"
+    echo ""
 
     # Add the balance to the total sum using bc for decimal arithmetic
-    total_sum=$(echo "$total_sum + $balance" + "$staked" | bc)
+    total_sum=$(echo "$total_sum + $balance" + "$appstaked" + "$nodestaked" | bc)
 
     # Print the current total sum
-    echo "Running total sum: $total_sum"
+    echo "Running total sum: $total_sum uPOKT"
+    total_sum_POKT=$(echo "scale=2 ; $total_sum / $conversion" | bc)
+    echo "Running total sum: $total_sum_POKT POKT"
+
 done < input.txt  # Change 'input.txt' to the name of your text file containing the list of addresses
 
